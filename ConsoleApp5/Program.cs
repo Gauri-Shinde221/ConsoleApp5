0f25c716a63ebfbb7928e57dd9071d2592d0ea23
Gauri-Shinde221 <gauriabashindegmail.com>
2025-07-25 10:29:53 +0530

    Added Add Method

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleApp5
 {
     class Program
     {
+        int c;
+        public void Add()
+        {
+            int b, a;
+            b = 20;
+            a = 10;
+            c = b + a; 
+            
+        }
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Add();
         }
     }
 }
